e06fa94bebac4d4d80b3ed399632a50b3b9a6f9d
Izzan Dienurrahman <izzan.dienurrahman@gmail.com>
2022-12-06 20:17:42 +0700

    submit NGC 6

---
+++ moduleTes.py
@@ -0,0 +1,11 @@
+def greeting(name):
+    print("Hallo, salam kenal " + name)
+
+rasa = lambda rasa:rasa
+print("rasa jeruk",rasa("manis"))
+
+person1 ={
+    "name":"John",
+    "age":36,
+    "country":"Norway"
+}
